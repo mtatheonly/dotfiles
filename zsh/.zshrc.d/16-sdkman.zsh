ced777aea6c8ab83b8c7048a479d42f8dec703b3
Marcos Tadeu de Andrade <marcos-tadeu.andrade@serpro.gov.br>
2020-02-06 11:35:53 -0300

    Adding sdkman and autosuggest as .zshrc.d

---
+++ zsh/.zshrc.d/16-sdkman.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+if [ -d "$HOME/.sdkman" ]; then
+    export SDKMAN_DIR="$HOME/.sdkman"
+    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
