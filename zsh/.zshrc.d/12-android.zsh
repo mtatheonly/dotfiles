896ea334fcb093f1501c91ff6f93e29be01d2fba
Tadeu Andrade <tadeu.andrade@gmail.com>
2020-02-06 10:32:44 -0300

    Cleaning

--- zsh/.zshrc.d/12-android.zsh
+++
@@ -1,5 +0,0 @@
-export ANDROID_HOME=$HOME/tools/lang/java/android
-export PATH=$ANDROID_HOME/tools:$PATH
-export PATH=$ANDROID_HOME/tools/bin:$PATH
-export PATH=$ANDROID_HOME/platform-tools:$PATH
-export PATH=$ANDROID_HOME/emulator:$PATH
