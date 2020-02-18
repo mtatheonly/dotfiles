6a1df4e9d5a0888a7ac707852006514836f597b9
Tadeu Andrade <tadeu.andrade@gmail.com>
2020-02-18 17:04:12 -0300

    (fix) docker-compose completions

--- .config/fish/functions/update-docker-compose-completions.fish
+++
@@ -1,3 +0,0 @@
-function update-docker-compose-completions --description "Update docker-compose completions"
-   curl -o $HOME/.config/fish/completions/docker-compose.fish -L# https://github.com/docker/compose/raw/master/contrib/completion/fish/docker-compose.fish
-end
