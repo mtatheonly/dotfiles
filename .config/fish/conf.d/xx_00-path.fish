c9cbe6f3589ff3fd66c9f019bd906223fd70ab12
Tadeu Andrade <tadeu.andrade@gmail.com>
2020-03-01 15:58:15 -0300

    (fix) init script preventing scp working as expecting

--- .config/fish/conf.d/xx_00-path.fish
+++ .config/fish/conf.d/xx_00-path.fish
@@ -1,4 +1,5 @@
-if not which fish
+which fish > /dev/null 2>&1
+if test $status -eq 0 
     if test -f $HOME/tools/productivity/fish/bin/fish
         set -gx PATH $HOME/tools/productivity/fish/bin $PATH
     end
