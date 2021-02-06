99398fe1834d52225d2d93e779a9d4ed7d258222
Tadeu Andrade <tadeu.andrade@gmail.com>
2021-02-06 16:39:02 -0300

    (fix) Add common directories to PATH

--- .config/fish/conf.d/00-local.fish
+++ .config/fish/conf.d/00-local.fish
@@ -3,3 +3,15 @@ if test -d $HOME/.config/fish/conf.d/local
 	. $i
    end
 end
+
+if test -d $HOME/.local/bin
+    if not contains -- $HOME/.local/bin $PATH
+            set -p PATH $HOME/.local/bin
+    end
+end
+
+if test -d $HOME/bin
+    if not contains -- $HOME/bin $PATH
+            set -p PATH $HOME/bin
+    end
+end
