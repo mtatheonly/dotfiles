d82b03ddcda8f04280af513582c114ebcbbd6407
Tadeu Andrade <tadeu.andrade@gmail.com>
2024-06-11 09:34:51 -0300

    feat: adding asdf tools

--- zsh/.zshrc.d/01-mise.zsh
+++ zsh/.zshrc.d/01-mise.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/zsh
-eval "$(/home/tadeu/.local/bin/mise activate zsh)"
-. <(mise completion zsh)
+
+if [ -f $HOME/.local/bin/mise ]; then
+  eval "$($HOME/.local/bin/mise activate zsh)"
+  . <(mise completion zsh)
+fi
